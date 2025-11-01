5df70c7679418d2396ac09e53c5aaec23d8fc7bd
Jaidil14 <jaidilrachmad@gmail.com>
2025-11-01 07:55:41 +0700

    Menghilangkan dependensi yang tidak diperlukan

--- app/src/main/java/com/example/questuserinput_099/Input.kt
+++ app/src/main/java/com/example/questuserinput_099/Input.kt
@@ -1,8 +1,6 @@
 package com.example.questuserinput_099
 
-import androidx.benchmark.traceprocessor.Row
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -11,7 +9,6 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.selection.selectable
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
@@ -29,7 +26,6 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ModifierLocalBeyondBoundsLayout
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
